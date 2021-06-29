41fa64ee41e421d72c2009e8ec6585b61666d0c4
Raymond <raymond@hetwebbureau.nl>
2021-06-29 23:25:45 +0200

    feat: program: whoami: info  copy  provided by agr, remove default  command and removed test method

--- src/programs/whoami.ts
+++ src/programs/whoami.ts
@@ -1,38 +1,24 @@
 import { Program } from './Program';
 import { ICommand } from '../interpret';
 
-export default new Program({
-    indentifier: {
-        name: 'whoami',
-    },
-    version: '1',
-    description: 'tells something about whoami',
-    methods: [
-        {
-            indentifier: {
-                name: 'info',
-                abbreviation: 'i',
-            },
-            default: true,
-            description: 'returns some information',
-            execute(command: ICommand) {
-                return Promise.resolve(`<p>
-                  Spare time project of Raymond Schweers.<br/>
-                  To explore, learn and seek new technologies and by doing so creating a handy<br/>
-                  command line interface build with: TypeScript, React, and Sass.<br/>
-                  version: 99
-                </p>`);
-            },
+export default ({ info }: { info: string }) => {
+    return new Program({
+        indentifier: {
+            name: 'whoami',
         },
-        {
-            indentifier: {
-                name: 'test',
-                abbreviation: 't',
-            },
-            description: 'tells something about this application',
-            execute(command: ICommand) {
-                return Promise.resolve(`<p>test runned..</p>`);
-            },
-        },
-    ],
-});
+        version: '1',
+        description: 'returns information about the current enviroment',
+        methods: [
+            {
+                indentifier: {
+                    name: 'info',
+                    abbreviation: 'i',
+                },
+                description: 'more information',
+                execute(command: ICommand) {
+                    return Promise.resolve(info);
+                },
+            }
+        ],
+    });
+};
