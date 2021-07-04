6bba04ad93f4a8e4c412a7df96f4cfd8c974fff7
Raymond <raymond@hetwebbureau.nl>
2021-07-04 21:49:10 +0200

    test: ProgramManager

---
+++ src/__tests__/ProgramManager.test.ts
@@ -0,0 +1,35 @@
+import { init, programs as defaultPrograms } from '../';
+import { Whoami } from '../programs/whoami';
+import { ProgramManagerProgram } from '../programs/ProgramManager';
+import { expectHelpMethod } from './program.test';
+
+const FAKE_INFO = 'FAKE_INFO';
+const whoami = defaultPrograms.whoami({
+    info: FAKE_INFO,
+});
+
+const programs = [whoami];
+
+export const interpret = init({
+    programs
+});
+
+const expectProgramManagerHelp = () => expectHelpMethod(interpret, ProgramManagerProgram(programs));
+
+describe('program', () => {
+    it('should return help as default', expectProgramManagerHelp);
+
+    it('should return help', expectProgramManagerHelp);
+
+    it('should return info', () => {
+        interpret('program list').then((res) => {
+            expect(res).toContain('whoami');
+        });
+    });
+
+    it('should return version', () => {
+        interpret('program version').then((res) => {
+            expect(res).toContain('1.0.0');
+        });
+    });
+});
--- src/programs/ProgramManager.ts
+++ src/programs/ProgramManager.ts
@@ -1,57 +1,57 @@
-import { programs } from './'
+import { programs } from './';
 import { Program, IProgram } from '../Program';
 import { ICommand } from '../interpret';
 
-const version = '1.0.0'
+const version = '1.0.0';
 
-export const ProgramManager = (userPrograms: IProgram[]) => {
+export const ProgramManagerProgram = (allPrograms:IProgram[]) => new Program({
+    indentifier: {
+        name: 'program',
+    },
+    version,
+    description: 'install, update and remove programs',
+    methods: [
+        {
+            indentifier: {
+                name: 'list',
+                abbreviation: 'l',
+            },
+            description: 'list all programs',
+            execute(command: ICommand) {
+                const list = allPrograms.reduce((accumulator: string, prog: IProgram): string => {
+                    const {
+                        version: programVersion,
+                        indentifier: { name },
+                    } = prog;
 
-    const allPrograms = [
-        ...userPrograms
-    ];
-
-    const program = new Program({
-        indentifier: {
-            name: 'program',
-        },
-        version,
-        description: 'install, update and remove programs',
-        methods: [
-            {
-                indentifier: {
-                    name: 'list',
-                    abbreviation: 'l',
-                },
-                description: 'list all programs',
-                execute(command: ICommand) {
-                    const list = allPrograms.reduce((accumulator: string, prog: IProgram): string => {
-                        const { version: programVersion, indentifier : { name }} = prog;
-
-                        return `${accumulator}
+                    return `${accumulator}
                             <tr>
                                 <td class="pr-5">${name} ${programVersion}</td>
                             </tr>`;
-                    }, '');
+                }, '');
 
-                    const response = (`<ul>${list}</ul>`);
+                const response = `<ul>${list}</ul>`;
 
-                    return Promise.resolve(response);
-                },
+                return Promise.resolve(response);
             },
-            {
-                indentifier: {
-                    name: 'version',
-                    abbreviation: 'v',
-                },
-                description: 'current installed version',
-                execute(command: ICommand) {
-                    return Promise.resolve(`v${version}`);
-                }
-            }
-        ]
-    });
+        },
+        {
+            indentifier: {
+                name: 'version',
+                abbreviation: 'v',
+            },
+            description: 'current installed version',
+            execute(command: ICommand) {
+                return Promise.resolve(`v${version}`);
+            },
+        },
+    ],
+});
+
+export const ProgramManager = (userPrograms: IProgram[]) => {
+    const allPrograms = [...userPrograms];
 
     return {
-        programs: [ ...allPrograms, program]
-    }
+        programs: [...allPrograms, ProgramManagerProgram(allPrograms)],
+    };
 };
